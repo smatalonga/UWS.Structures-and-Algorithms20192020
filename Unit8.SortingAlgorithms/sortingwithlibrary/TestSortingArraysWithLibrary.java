2db8cab22df3781bbf6fe0a88edd403c2cbc2586
Santiago Matalonga <smatalonga@gmail.com>
2019-11-18 09:48:42 +0000

    Unit 8
    Unit 8. sorting algorithms

---
+++ Unit8.SortingAlgorithms/sortingwithlibrary/TestSortingArraysWithLibrary.java
@@ -0,0 +1,24 @@
+package com.uws.saa.sortingwithlibrary;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.uws.saa.sortingarrays.SortingAlgorithmsForintArrays;
+
+class TestSortingArraysWithLibrary {
+
+	@Test
+	void testSelectionSort() {
+		
+		
+		Integer [] myarray = new Integer[] {99, 38, 80,48};
+		
+		Integer [] myExpectedSortedarray = new Integer[] {38, 48, 80, 99};
+		
+		SelectionSort.sort(myarray);
+		
+		assertArrayEquals(myExpectedSortedarray, myarray);	
+	}
+
+}
