0872cc45c198ccd89f74b87e9f526df95eda638d
Santiago Matalonga <smatalonga@gmail.com>
2020-01-29 11:56:49 +0000

    Updated code from Wednesday 29

---
+++ SYA.Unit9.LinkedLists/src/com/uws/linkedlist/secondClass/SortedADT.java
@@ -0,0 +1,14 @@
+package com.uws.linkedlist.secondClass;
+// this is a cut down version of the interface specifically for the Linear Search test
+// the complete version is in the Library folder on Moodle
+
+public abstract interface SortedADT {
+    
+    public class NotFoundException extends Exception{}
+
+    public abstract void insert (Comparable object);
+
+    public abstract Comparable find(Comparable object) throws NotFoundException;
+
+	public abstract int countElements();
+}
